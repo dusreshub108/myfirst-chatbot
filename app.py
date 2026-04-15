d6b77ef9e94c970a7324ba1cdbb0b0fce1e80b6d
Shubham Dusre <dusreshub108@gmail.com>
2026-04-15 20:52:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # Page Config
 # -----------------------------
 st.set_page_config(
-    page_title="My First Chatbot",
+    page_title="QueryMind",
     page_icon="🤖",
     layout="wide"
 )
@@ -38,7 +38,7 @@ st.markdown("""
 # -----------------------------
 # Header
 # -----------------------------
-st.title("CDS Confluence Chatbot")
+st.title("QueryMind")
 st.caption("Ask questions from your documents")
 
 # -----------------------------
